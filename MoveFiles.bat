e5673b10a77fe261f7475c05050a2873f6dbaa83
Sebastian <js.gomez23@uniandes.edu.co>
2020-12-13 22:34:53 -0500

    pushing oracle

--- MoveFiles.bat
+++ MoveFiles.bat
@@ -16,4 +16,12 @@ FOR  %%G  IN (*.txt) DO MOVE %%G "C:\Users\juseg\Documents\Pruebas\Practico2\ora
 ::FOR  %%G IN (*.html) DO MOVE %%G "C:\Users\juseg\Documents\Pruebas\Practico2\mutant%1"
 FOR  %%G IN (*.html) DO MOVE %%G "C:\Users\juseg\Documents\Pruebas\Practico2\oracle"
 
+git status
+
+git add .
+git commit -m"pushing oracle"
+:: git commit -m "pushing mutant%1"
+git pull
+git push
+
 echo finished
